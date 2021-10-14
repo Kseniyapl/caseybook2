bef18b62bfc4c397b67ad8808c737c8cee90516c
Kseniya Platonava <43353550+Kseniyapl@users.noreply.github.com>
2021-10-14 17:59:57 -0400

    Add API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import logo from './logo.svg';
+import React, {useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
+
+//user.name -> user.first + user.last, 
+// user.location, 
+// user.uuid, 
+// user.picture.medium, 
+// user.post ->streach, 
+// user.email
 
 function App() {
+  const [user, setUser] = useState([])
+
+  useEffect (() => {
+    axios.get('https://randomuser.me/api/')
+    .then(res =>{
+      console.log(res)
+    }).catch(err => console.error(err))
+
+  }, [])
+
   return (
     <div className="App">
       <h1>  
-        Kseniya PLatonava
+        Kseniya Platonava
       </h1>
       <h2>Comming Soon</h2>
 
